75a1acfc5d69a71c74e5094256c685e9e358e062
alecsechechel <iosOleksii@gmail.com>
2015-08-03 12:56:47 +0300

    pod install

--- AutoLogin/AutoLogin/ViewController.swift
+++ AutoLogin/AutoLogin/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usetTextField: UITextField!
+    
+    //MARK: UIViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
