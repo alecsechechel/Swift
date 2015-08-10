0a7cc91ca81021320132794ca44dcf65b81bb247
alecsechechel <iosOleksii@gmail.com>
2015-08-10 09:52:51 +0300

    init

---
+++ API_Moya/API_MoyaTests/API_MoyaTests.swift
@@ -0,0 +1,36 @@
+//
+//  API_MoyaTests.swift
+//  API_MoyaTests
+//
+//  Created by admin on 10.08.15.
+//  Copyright (c) 2015 brocoders. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class API_MoyaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
